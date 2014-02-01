c5efd4aa61fe0988cc2d139b1d9734a14bf4bc82
Emmanuel Quentin <manu.quentin@gmail.com>
2014-02-02 00:10:02 +0100

    Use grunt.files.expand, allowing to be cross-platform

--- tasks/include.js
+++ tasks/include.js
@@ -13,82 +13,81 @@ var replaceContent = require('./../replaceContent.js');
 
 module.exports = function(grunt) {
 
-		var self = this;
+    var self = this;
 
-		grunt.registerMultiTask('include', 'Layout templating', function(){
-				var files = execSync('ls '+this.data).split("\n");
+    grunt.registerMultiTask('include', 'Layout templating', function(){
+        var files = grunt.file.expand(this.data);
 
-				for(var i = 0, nbFiles = files.length; i < nbFiles; i++){
-						var file = files[i];
-						var content = grunt.file.read(file);
+        for(var i = 0, nbFiles = files.length; i < nbFiles; i++){
+            var file = files[i];
+            var content = grunt.file.read(file);
 
-						var fileData = file.split('/');
-						fileData.pop();
-						var fileDir = fileData.join('/')+'/';
+            var fileData = file.split('/');
+            fileData.pop();
+            var fileDir = fileData.join('/')+'/';
 
-						content = replaceContent(content, fileDir);
+            content = replaceContent(content, fileDir);
 
-						grunt.file.write(file, content);
-				}
-		});
+            grunt.file.write(file, content);
+        }
+    });
 
-		grunt.registerMultiTask('include:clean', 'Clean layout templating', function(){
-				if(this.files.length == 0){
-						return;
-				}
+    grunt.registerMultiTask('include:clean', 'Clean layout templating', function(){
+        if(this.files.length == 0){
+            return;
+        }
 
-				var regexp = /(<!--\s+\[include:[^\]]+\]\s+-->)(?:\r|\n|.)+?<!--\s+\\\[include:[^\]]+\]\s+-->/g;
-				var contentRegexp = /(<!--\s+\[include:[^\]]+\]\s+-->)((?:\r|\n|.)+?)<!--\s+\\\[include:[^\]]+\]\s+-->/;
+        var regexp = /(<!--\s+\[include:[^\]]+\]\s+-->)(?:\r|\n|.)+?<!--\s+\\\[include:[^\]]+\]\s+-->/g;
+        var contentRegexp = /(<!--\s+\[include:[^\]]+\]\s+-->)((?:\r|\n|.)+?)<!--\s+\\\[include:[^\]]+\]\s+-->/;
+        var files = grunt.file.expand(this.data);
 
-				var files = execSync('ls '+this.data).split("\n");
+        // Clean app files
+        for(var i = 0, nbFiles = files.length; i < nbFiles; i++){
+            var file = files[i];
+            var content = grunt.file.read(file);
 
-				// Clean app files
-				for(var i = 0, nbFiles = files.length; i < nbFiles; i++){
-						var file = files[i];
-						var content = grunt.file.read(file);
+            var layouts = content.match(regexp);
+            if(!layouts || !layouts.length){
+                return;
+            }
 
-						var layouts = content.match(regexp);
-						if(!layouts || !layouts.length){
-								return;
-						}
+            for(var j = 0, length = layouts.length; j < length; j++){
+                var layoutContent = layouts[j].match(contentRegexp);
 
-						for(var j = 0, length = layouts.length; j < length; j++){
-								var layoutContent = layouts[j].match(contentRegexp);
+                content = content.split(layouts[j]).join(layoutContent[1]);
+            }
 
-								content = content.split(layouts[j]).join(layoutContent[1]);
-						}
+            grunt.file.write(file, content);
+        }
+    });
 
-						grunt.file.write(file, content);
-				}
-		});
+    grunt.registerMultiTask('include:clean-dest', 'Clean final layout templating', function(){
+        if(this.files.length == 0){
+            return;
+        }
 
-		grunt.registerMultiTask('include:clean-dest', 'Clean final layout templating', function(){
-				if(this.files.length == 0){
-						return;
-				}
+        var regexp = /(<!--\s+\[include:[^\]]+\]\s+-->)(?:\r|\n|.)+?<!--\s+\\\[include:[^\]]+\]\s+-->/g;
+        var contentRegexp = /<!--\s+\[include:[^\]]+\]\s+-->((?:\r|\n|.)+?)<!--\s+\\\[include:[^\]]+\]\s+-->/;
 
-				var regexp = /(<!--\s+\[include:[^\]]+\]\s+-->)(?:\r|\n|.)+?<!--\s+\\\[include:[^\]]+\]\s+-->/g;
-				var contentRegexp = /<!--\s+\[include:[^\]]+\]\s+-->((?:\r|\n|.)+?)<!--\s+\\\[include:[^\]]+\]\s+-->/;
+        var files = execSync('ls '+this.data).split("\n");
 
-				var files = execSync('ls '+this.data).split("\n");
+        // Clean app files
+        for(var i = 0, nbFiles = files.length; i < nbFiles; i++){
+            var file = files[i];
+            var content = grunt.file.read(file);
 
-				// Clean app files
-				for(var i = 0, nbFiles = files.length; i < nbFiles; i++){
-						var file = files[i];
-						var content = grunt.file.read(file);
+            var layouts = content.match(regexp);
+            if(!layouts || !layouts.length){
+                return;
+            }
 
-						var layouts = content.match(regexp);
-						if(!layouts || !layouts.length){
-								return;
-						}
+            for(var j = 0, length = layouts.length; j < length; j++){
+                var layoutContent = layouts[j].match(contentRegexp);
 
-						for(var j = 0, length = layouts.length; j < length; j++){
-								var layoutContent = layouts[j].match(contentRegexp);
+                content = content.split(layouts[j]).join(layoutContent[1]);
+            }
 
-								content = content.split(layouts[j]).join(layoutContent[1]);
-						}
-
-						grunt.file.write(file, content);
-				}
-		});
+            grunt.file.write(file, content);
+        }
+    });
 };
